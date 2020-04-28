a62b51d20b080daed7cd7531b48010b2934dbc21
Dominik.Kotecki <dominikus1910@gmail.com>
2020-04-28 21:57:07 +0200

    return data from github

--- src/Server/Infrastructure/Github.fs
+++ src/Server/Infrastructure/Github.fs
@@ -1,67 +1,77 @@
 namespace Infrastructure
+
 open FSharp.Control
 
 module Github =
     open System
     open FSharp.Data
+    open Domain.Types
 
-    type Record =
+    type private Record =
         { CountryRegion: string
           ProvinceState: string
           LastUpdate: DateTime
           Confirmed: int
           Gain: int }
 
-    let firstReportDate = DateTime(2020, 1, 22).AddDays(0.)
-    let lastReportDate = DateTime.Today
+    let private firstReportDate = DateTime(2020, 1, 22).AddDays(0.)
+    let private lastReportDate = DateTime.Today
 
-    let dateCount = (lastReportDate - firstReportDate).Days
+    let private dateCount = (lastReportDate - firstReportDate).Days
 
-    let reportBaseUrl =
+    let private reportBaseUrl =
         "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
-    let toDailyReportUrl (dt: DateTime) = sprintf "%s%s.csv" reportBaseUrl (dt.ToString("MM-dd-yyyy"))
+    let private toDailyReportUrl (dt: DateTime) = sprintf "%s%s.csv" reportBaseUrl (dt.ToString("MM-dd-yyyy"))
 
-    let dates = seq [ 0 .. dateCount - 1 ] |> Seq.map (float >> firstReportDate.AddDays)
+    let private dates = seq [ 0 .. dateCount - 1 ] |> Seq.map (float >> firstReportDate.AddDays)
 
-    let dailyReportUrls =
-        dates
-        |> Seq.map toDailyReportUrl
+    let private dailyReportUrls = dates |> Seq.map toDailyReportUrl
 
 
-    let toShortDate (dt: DateTime) = dt.ToShortDateString()
-    let noTime (dt: DateTime) = DateTime(dt.Year, dt.Month, dt.Day)
+    let private toShortDate (dt: DateTime) = dt.ToShortDateString()
+    let private noTime (dt: DateTime) = DateTime(dt.Year, dt.Month, dt.Day)
 
-    let parseConfirmed =
+    let private parseConfirmed =
         function
         | "" -> 0
         | str -> int str
 
-    let parseRecord (row: CsvRow) =
+    let private parseRecord (row: CsvRow) =
         try
-            Some({ CountryRegion = row.["Country/Region"]
+            Some
+                ({ CountryRegion = row.["Country/Region"]
                    ProvinceState = row.["Province/State"]
                    LastUpdate = row.["Last Update"].AsDateTime() |> noTime
                    Confirmed = row.["Confirmed"] |> parseConfirmed
                    Gain = 0 })
-        with ex ->
-            None
+        with ex -> None
 
 
-    let loadCsv url =
+    let private loadCsv url =
         async {
-             try
+            try
                 let! data = CsvFile.AsyncLoad url
                 return data |> Some
-             with ex ->
-                return None
+            with ex -> return None
         }
 
     let loadData() =
         dailyReportUrls
-            |> AsyncSeq.ofSeq
-            |> AsyncSeq.mapAsync(loadCsv)
-            |> AsyncSeq.choose id
-            |> AsyncSeq.collect (fun x -> x.Rows |> AsyncSeq.ofSeq)
-            |> AsyncSeq.map(parseRecord)
-            |> AsyncSeq.choose id
-
+        |> AsyncSeq.ofSeq
+        |> AsyncSeq.mapAsync (loadCsv)
+        |> AsyncSeq.choose id
+        |> AsyncSeq.collect (fun x -> x.Rows |> AsyncSeq.ofSeq)
+        |> AsyncSeq.map (parseRecord)
+        |> AsyncSeq.choose id
+        |> AsyncSeq.groupBy (fun x -> x.CountryRegion)
+        |> AsyncSeq.map (fun (country, data) ->
+            { CountryName = country
+              Location =
+                  { Longitude = 21.017532
+                    Latitude = 52.237049 }
+              WholeCountryCases =
+                  seq
+                      [ { Date = DateTime.Now
+                          Confirmed = 10
+                          Gain = -1 } ]
+              Provinces = None })
--- src/Server/Infrastructure/Storage.fs
+++ src/Server/Infrastructure/Storage.fs
@@ -3,21 +3,12 @@ namespace Infrastructure
 open System
 open Domain
 open Domain.Types
+open FSharp.Control
 
 module Storage =
 
     let getCovidCases(): Async<CovidCases> =
         async {
-            return Some
-                       (seq
-                           [ { CountryName = "Poland"
-                               Location =
-                                   { Longitude = 21.017532
-                                     Latitude = 52.237049 }
-                               WholeCountryCases =
-                                   seq
-                                       [ { Date = DateTime.Now
-                                           Confirmed = 10
-                                           Gain = -1 } ]
-                               Provinces = None } ])
+            let! data = Github.loadData() |> AsyncSeq.toArrayAsync
+            return Some(data |> Array.toSeq)
         }
